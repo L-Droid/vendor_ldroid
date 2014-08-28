SlimRoms/vendor_slim

vendor_slim: Update Nova Launcher to latest version
Update Nova Launcher to version 3.0. Also, add libgif.so to
prebuilt libs.

Change-Id: If38f01fa8fa0f54f2cc5410f9982d30a7b0e01d2

config/common.mk
@@ -40,6 +40,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/slim/prebuilt/common/lib/libjni_latinime.so:system/lib/libjni_latinime.so
 
+# Copy libgif for Nova Launcher 3.0
+PRODUCT_COPY_FILES += \
+    vendor/slim/prebuilt/common/lib/libgif.so:system/lib/libgif.so
+
 # SELinux filesystem labels
 PRODUCT_COPY_FILES += \
     vendor/slim/prebuilt/common/etc/init.d/50selinuxrelabel:system/etc/init.d/50selinuxrelabel

L-Droid/vendor_ldroid

Conflicts:
	config/common.mk
	prebuilt/common/app/NovaLauncher.apk

config/common.mk
@@ -102,6 +102,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/ldroid/prebuilt/common/etc/init.d/90userinit:system/etc/init.d/90userinit
 
+# Copy libgif for Nova Launcher 3.0
+PRODUCT_COPY_FILES += \
+    vendor/ldroid/prebuilt/common/lib/libgif.so:system/lib/libgif.so
+
 # CM-specific init file
 PRODUCT_COPY_FILES += \
     vendor/ldroid/prebuilt/common/etc/init.local.rc:root/init.ldroid.rc